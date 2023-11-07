2c57ce94b261deb801d2a77f11e24f2cbf6e5a97
chenkai <1078410321@qq.com>
2023-11-07 22:02:01 +0800

    add common Command

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,6 +1,24 @@
 'use strict';
 
-function init(projectName, cmdObj) {
+const Command = require('@immoc-cli-dev-zed/command');
+const log = require('@immoc-cli-dev-zed/log');
+class InitCommand extends Command {
+    init() {
+        this.projectName = this._argv[0] || '';
+        this.force = this._cmd.opts().force;
+        log.verbose('projectName', this.projectName);
+        log.verbose('force', this.force);
+    }
+
+    exec() {
+        console.log('exec');
+    }
 }
 
+function init(argv) {
+    return new InitCommand(argv);
+}
+
+
 module.exports = init;
+module.exports.InitCommand = InitCommand;
