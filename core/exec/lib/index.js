6fc23cc107810556ceb189c01c917f7f8875d000
chenkai <1078410321@qq.com>
2023-11-08 21:38:21 +0800

    使用node多进程去执行命令

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -3,6 +3,7 @@
 const Package = require('@immoc-cli-dev-zed/package');
 const log = require('@immoc-cli-dev-zed/log');
 const path = require('path');
+const cp = require('child_process');
 
 const SETTINGS = {
     init: '@immoc-cli-dev-zed/init'
@@ -54,12 +55,47 @@ async function exec() {
     const rootFile = pkg.getRootFilePath(); 
     if (rootFile) {
         try {
-            require(rootFile).call(null, Array.from(arguments));
+            //在当前进程中调用
+            // require(rootFile).call(null, Array.from(arguments));
+            //在node子进程调用
+            const args = Array.from(arguments);
+            const cmd = args[args.length - 1];
+            const obj = Object.create(null);
+            Object.keys(cmd).forEach(key => {
+                
+                if (cmd.hasOwnProperty(key) &&
+                    !key.startsWith('_') &&
+                    key !== 'parent') {
+                    obj[key] = cmd[key];
+                    }
+            })
+
+            args[args.length - 1] = obj;
+            const code = `require('${rootFile}').call(null, ${JSON.stringify(args)})`;
+            const child = spawn('node', ['-e', code], {
+                cwd: process.cwd(),
+                stdio: 'inherit'
+            });
+            child.on('error', e => {
+                log.error(e.message);
+                process.exit(1);
+            })
+            child.on('exit', e => {
+                log.verbose('命令执行成功: ' + e);
+                process.exit(e);
+            })
         } catch (error) {
             log.error(error.message);   
         }
-    }
-    
+    } 
+}
+
+function spawn(command, args, options) {
+    const win32 = process.platform === 'win32';
+
+    const cmd = win32 ? 'cmd' : command;
+    const cmdArgs = win32 ? ['/c'].concat(command, args) : args;
+    return cp.spawn(cmd, cmdArgs, options || {});
 }
 
 module.exports = exec;
